251e23a240a3f3c3dc41133237eb46a4fab8d9fb
N Naman Kumar <145587550+Namanreddy09@users.noreply.github.com>
2025-02-27 10:22:14 +0530

    Update DeveloperActions.java

--- EMS/src/Employee/DeveloperActions.java
+++ EMS/src/Employee/DeveloperActions.java
@@ -1,7 +1,7 @@
 package Employee;
 
 //Creating a specific Interface which uses  Interface Segregation Principle(ISP)
-
+// adding a new comment 
 public interface DeveloperActions 
 {
 	void writeCode();      // Developer-specific action
